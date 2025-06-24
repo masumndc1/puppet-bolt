83dd64960c8f7019c5c4fb01a2bc479dc5bd466b
Khabir Uddin <khuddin@csc.fi>
2025-06-24 15:34:25 +0300

    puppet manifest file:

---
+++ example.pp
@@ -0,0 +1,7 @@
+class example {
+  $hostname = $facts['networking']['hostname']
+  notice("Running on host: ${hostname}")
+  notify { 'This is a notification message':
+    message => 'Apache has been installed!',
+  }
+}
