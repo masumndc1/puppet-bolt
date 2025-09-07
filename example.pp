ebffb194367d7d6213bf62a3815e59e1b10ca71c
Khabir Uddin <khuddin@csc.fi>
2025-09-07 12:53:58 +0300

    some files have changed their location

--- example.pp
+++
@@ -1,7 +0,0 @@
-class example {
-  $hostname = $facts['networking']['hostname']
-  notice("Running on host: ${hostname}")
-  notify { 'This is a notification message':
-    message => 'Apache has been installed!',
-  }
-}
--- partition.pp
+++
@@ -1,6 +0,0 @@
-class partition {
-# checking partition and their size
-  each ( $facts['partitions'] ) |$name, $capacity | {
-    notice("partition ${name} has size of ${capacity['size']}")
-  }
-}
