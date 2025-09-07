fc5cefff7959b1afc8fb19969151f00e6870b234
Khabir Uddin <khuddin@csc.fi>
2025-09-07 12:50:15 +0300

    stand alone manifest files

---
+++ manifests/neovim.pp
@@ -0,0 +1,3 @@
+package { 'neovim':
+  ensure => installed,
+}
---
+++ manifests/pkg.pp
@@ -0,0 +1,17 @@
+if ($facts['os']['distro']['id'] == "Debian") {
+  apt::keyring { 'puppetlabs-keyring.gpg':
+    source => 'https://apt.puppetlabs.com/keyring.gpg',
+  }
+}
+
+if ($facts['os']['distro']['release']['major'] in ['8','9','10']) {
+  # https://dl.fedoraproject.org/pub/epel/epel-release-latest-8.noarch.rpm
+  # https://dl.fedoraproject.org/pub/epel/epel-release-latest-9.noarch.rpm
+  # https://dl.fedoraproject.org/pub/epel/epel-release-latest-10.noarch.rpm
+  $source = "https://dl.fedoraproject.org/pub/epel/epel-release-latest-${facts['os']['distro']['release']['major']}.noarch.rpm"
+
+  yum::install { 'epel-release':
+    ensure => present,
+    source => $source,
+  }
+}
