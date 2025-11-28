608ab4b815aac94d3bee25616d08b1f7ad3a84f1
Khabir Uddin <khuddin@csc.fi>
2025-11-28 14:55:37 +0200

    if you want to run a single manifest file with bolt
    then you need to rename that file in init.pp file

---
+++ manifests/init.pp
@@ -0,0 +1,23 @@
+if ($facts['os']['distro']['id'] =~ "Debian|Ubuntu") {
+  apt::keyring { 'puppetlabs-keyring.gpg':
+    source => 'https://apt.puppetlabs.com/keyring.gpg',
+  }
+
+  package { 'python3':
+    ensure => installed,
+  }
+}
+
+if ($facts['os']['distro']['release']['major'] in ['8','9','10']) {
+  # https://dl.fedoraproject.org/pub/epel/epel-release-latest-8.noarch.rpm
+  # https://dl.fedoraproject.org/pub/epel/epel-release-latest-9.noarch.rpm
+  # https://dl.fedoraproject.org/pub/epel/epel-release-latest-10.noarch.rpm
+  $source = "https://dl.fedoraproject.org/pub/epel/epel-release-latest-${facts['os']['distro']['release']['major']}.noarch.rpm"
+
+  yum::install { 'epel-release':
+    ensure => present,
+    source => $source,
+  }
+}
+
+include uptime
