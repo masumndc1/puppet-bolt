2aeb6d6132db3e727858c73d8652246110e67c6b
Khabir Uddin <khuddin@csc.fi>
2025-11-28 16:14:13 +0200

    pkg plan

---
+++ plans/pkg.pp
@@ -0,0 +1,31 @@
+plan practise::pkg (
+  TargetSpec $nodes,
+) {
+
+  apply_prep($nodes)
+  $report = apply($nodes) {
+    if ($facts['os']['distro']['id'] =~ "Debian|Ubuntu") {
+      apt::keyring { 'puppetlabs-keyring.gpg':
+        source => 'https://apt.puppetlabs.com/keyring.gpg',
+      }
+
+      package { 'python3':
+        ensure => installed,
+      }
+    }
+
+    # RHEL-family 8/9/10
+    elsif $facts['os']['family'] =~ "RedHat" {
+      $os_major = $facts['os']['release']['major']
+      $source = "https://dl.fedoraproject.org/pub/epel/epel-release-latest-${os_major}.noarch.rpm"
+    #  $source = "https://dl.fedoraproject.org/pub/epel/epel-release-latest-9.noarch.rpm"
+
+      yum::install { 'epel-release':
+        ensure => present,
+        source  => $source,
+      }
+    }
+  }
+  return $report
+}
+
