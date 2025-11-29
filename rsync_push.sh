5bc06d9b9bbc9ae5a2072ac0e5ac0417c203094d
Khabir Uddin <khuddin@csc.fi>
2025-11-29 10:16:25 +0200

    pass env number to rsync script

--- rsync_push.sh
+++ rsync_push.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-rsync -avz -progress ~/Documents/github/puppet-bolt -e "ssh -F ssh.config" masum@sys-deb12-dev6:/home/masum/
-rsync -avz -progress ~/Documents/github/puppet-bolt -e "ssh -F ssh.config" masum@sys-alma9-dev6:/home/masum/
-rsync -avz -progress ~/Documents/github/puppet-bolt -e "ssh -F ssh.config" masum@sys-ubu22-dev6:/home/masum/
+rsync -avz -progress ~/Documents/github/puppet-bolt -e "ssh -F ssh.config" masum@sys-deb12-dev"$1":/home/masum/
+rsync -avz -progress ~/Documents/github/puppet-bolt -e "ssh -F ssh.config" masum@sys-alma9-dev"$1":/home/masum/
+rsync -avz -progress ~/Documents/github/puppet-bolt -e "ssh -F ssh.config" masum@sys-ubu22-dev"$1":/home/masum/
