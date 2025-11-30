9d2a677768026125ab34a35ec7958b870cc25529
Khabir Uddin <khuddin@csc.fi>
2025-11-30 09:44:21 +0200

    new manifests are added to base module

--- modules/base/manifests/init.pp
+++ modules/base/manifests/init.pp
@@ -1,5 +1,10 @@
 class base {
+  #include base::uptime
+  #include base::firewall
   include base::pkg
-  include base::uptime
   include base::summary
+  include base::neovim
+  include base::command
+  include base::partition
+  include base::greet
 }
