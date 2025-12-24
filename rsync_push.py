c4ce435289e7c01a7a6804bb8672db8038009004
Khabir Uddin <khuddin@csc.fi>
2025-12-25 00:30:35 +0200

    python scripts to rsync code to other nodes

---
+++ rsync_push.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+
+import sys
+import subprocess
+
+USER = "masum"
+NODES = ["alma9", "deb12", "ubu22"]
+LOC = " ~/Documents/github/puppet-bolt"
+
+
+def cmds(node, num):
+    user = USER
+    loc = LOC
+    cmd = (
+        "rsync -avz -progress"
+        + loc
+        + " -e 'ssh -F ssh.config'"
+        + " {}@sys-{}-dev{}:~".format(user, node, num)
+    )
+
+    subprocess.call(cmd, shell=True)
+
+
+def main():
+    if len(sys.argv) == 2:
+        num = sys.argv[1]
+    else:
+        print("python3 rsync_push.py 3")
+        sys.exit()
+
+    for node in NODES:
+        cmds(node, num)
+
+
+if __name__ == "__main__":
+    main()
