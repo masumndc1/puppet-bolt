aefe85720ef53befe1eebf7c4a1d960d5468894f
Khabir Uddin <khuddin@csc.fi>
2026-01-08 16:20:15 +0200

    deploys keys to incus nodes

--- plans/ssh_keys.pp
+++ plans/ssh_keys.pp
@@ -6,9 +6,20 @@ plan practise::ssh_keys (
     $user = lookup('user', default_value => [])
     $keys = lookup('keys')
 
+    package { 'sudo': ensure => installed, }
+
+    file { "/etc/sudoers.d/10_${user}":
+      content => "${user} ALL=(ALL:ALL) NOPASSWD: ALL",
+      require => Package['sudo'],
+      mode => '0440',
+      validate_cmd => "sudo visudo -c",
+    }
+
     file { "/home/$user/.ssh/authorized_keys":
       content => $keys.join("\n")
     }
+
+
   }
   # return $report
 }
