9a168cca23685dda26efc7f9a3fcfc00f31f2067
Khabir Uddin <khuddin@csc.fi>
2026-05-02 00:07:42 +0300

    install packages to keystone

--- plans/keystone.pp
+++ plans/keystone.pp
@@ -3,6 +3,16 @@ plan practise::keystone (
 ) {
   apply_prep($nodes)
   $report = apply($nodes) {
+  $pkgs = lookup('pkgs', merge => 'unique', default_value => [])
+
+  if $facts['os']['family'] =~ "RedHat" {
+    $pkgs.each | $pkg | {
+      package { "${pkg}":
+        name => $pkg,
+        ensure => installed,
+      }
+    }
+  }
 
   # define all lookup here
   include 'mysql::client'
