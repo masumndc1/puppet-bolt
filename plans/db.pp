7b992d2597ecb12af27cb9a387f20a2651ba34db
Khabir Uddin <khuddin@csc.fi>
2026-05-02 15:26:08 +0300

    create database for glance

--- plans/db.pp
+++ plans/db.pp
@@ -6,6 +6,7 @@ plan practise::db (
 
   # lookups here
   $kstone_db_pass = lookup('keystone_db_pass', default_value => [])
+  $glance_db_pass = lookup('glance_db_pass', default_value => [])
 
   include 'firewalld'
 
@@ -37,7 +38,15 @@ plan practise::db (
     collate  => 'utf8_general_ci',
   }
 
-
+  # define the mysql database for glance
+  mysql::db { 'glance':
+    user     => 'glance',
+    password => "${glance_db_pass}",
+    host     => 'localhost',
+    grant    => ['ALL'],
+    charset  => 'utf8',
+    collate  => 'utf8_general_ci',
+  }
   }
   return $report
 }
